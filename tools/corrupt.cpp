c7a4ee6f1509c985a95841075f513c7d5bec1ffe
Marc Schoolderman <squell@xs4all.nl>
2015-02-07 15:47:14 +0100

    correct solution with fast i/o (faster than the original C)

--- tools/corrupt.cpp
+++ tools/corrupt.cpp
@@ -8,10 +8,13 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     string buf;
     srand(time(0));
     while(getline(cin, buf)) {
 	if(!buf.empty()) buf[rand()%buf.length()] ^= 1;
-	cout << buf << endl;
+	cout.rdbuf()->sputn(buf.data(), buf.length());
+	cout.rdbuf()->sputc('\n');
     }
 }
