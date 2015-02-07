6da5c3bb1ef12ce6610834ab332197bbe1025120
Marc Schoolderman <squell@xs4all.nl>
2015-02-07 22:30:18 +0100

    touch test

--- demo/nfa_test.cpp
+++ demo/nfa_test.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <string>
 #include "../nfa/fuzzy_nfa.h"
+#include "../nfa/myers_nfa.h"
+#include "../nfa/limex_nfa.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    fuzzy_nfa<4> fsm(argc>1? argv[1] : "barbapappa");
+    typedef limex_nfa<4> nfa;
+    nfa fsm(argc>1? argv[1] : "[bp]ar*[bp]a[pf]a[pf][pb]a+");
 
     string word;
     while(cin >> word) {
-	fuzzy_nfa<4>::state st = fsm.start();
+	nfa::state st = fsm.start();
 	for(string::iterator p=word.begin(); p!=word.end(); ++p) {
 	    st.feed(fsm, *p);
 	}
